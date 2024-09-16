5577568aa0671b999c9ef3b1260421aba1d9c7b8
R06NV4LDR <ronny.bruhin+github@hotmail.com>
2024-09-16 16:32:34 +0200

    WhoLikesIt MySolution & BestPractice

--- src/WhoLikesIt/BestPractice.java
+++ src/WhoLikesIt/BestPractice.java
@@ -6,8 +6,8 @@ public class BestPractice {
             case 0: return "no one likes this";
             case 1: return String.format("%s likes this",names[0]);
             case 2: return String.format("%s and %s like this",names[0], names[1]);
-            case 3: return names[0] + " and " + names[1] + " and " + names[2] + " likes this";
-            default: return names[0] + " and " + names[1] + " and " + (names.length - 2) + " likes this";
+            case 3: return String.format(names[0] + " and " + names[1] + " and " + names[2] + " likes this");
+            default: return String.format(names[0] + " and " + names[1] + " and " + (names.length - 2) + " likes this");
         }
     }
 }
