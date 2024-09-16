41ea632740073d216aac51d2312b1559146bcf7b
Ronny A. Bruhin <ronny.bruhin+github@hotmail.com>
2024-09-16 22:23:01 +0200

    _02_WhoLikesIt updated

--- src/_02_WhoLikesIt/MySolution.java
+++ src/_02_WhoLikesIt/MySolution.java
@@ -9,11 +9,11 @@ public class MySolution {
             case 1:
                 return names[0] + " likes this";
             case 2:
-                return names[0] + " and " + names[1] + " likes this";
+                return names[0] + " and " + names[1] + " like this";
             case 3:
-                return names[0] + " and " + names[1] + " and " + names[2] + " likes this";
+                return names[0] + ", " + names[1] + " and " + names[2] + " like this";
             default:
-                return names[0] + " and " + names[1] + " and " + (names.length - 2) + " likes this";
+                return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
         }
     }
 }
---
+++ tests/WhoLikesItTest.java
@@ -0,0 +1,22 @@
+import _02_WhoLikesIt.BestPractice;
+import _02_WhoLikesIt.MySolution;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WhoLikesItTest {
+
+    @Test
+    public void staticTests() {
+        assertEquals("no one likes this", MySolution.whoLikesIt());
+        assertEquals("Peter likes this", MySolution.whoLikesIt("Peter"));
+        assertEquals("Jacob and Alex like this", MySolution.whoLikesIt("Jacob", "Alex"));
+        assertEquals("Max, John and Mark like this", MySolution.whoLikesIt("Max", "John", "Mark"));
+        assertEquals("Alex, Jacob and 2 others like this", MySolution.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+        assertEquals("no one likes this", BestPractice.whoLikesIt());
+        assertEquals("Peter likes this", BestPractice.whoLikesIt("Peter"));
+        assertEquals("Jacob and Alex like this", BestPractice.whoLikesIt("Jacob", "Alex"));
+        assertEquals("Max, John and Mark like this", BestPractice.whoLikesIt("Max", "John", "Mark"));
+        assertEquals("Alex, Jacob and 2 others like this", BestPractice.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+    }
+}
