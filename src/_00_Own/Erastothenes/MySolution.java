bbe8e832f31b978bc6fcc76fa972463ff5206f19
R06NV4LDR <ronny.bruhin+github@hotmail.com>
2024-09-17 15:09:04 +0200

    _00_Own.Erastothenes | first try

--- src/_00_Own/Erastothenes/MySolution.java
+++ src/_00_Own/Erastothenes/MySolution.java
@@ -1,13 +1,16 @@
 package _00_Own.Erastothenes;
 
 public class MySolution {
-    public static int sieve(int[] array) {
-        int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,};
+    public static int sieve(int[] numbers) {
+        numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,};
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; numbers >= i; ++i) {
             int product = i * i;
             return product;
-            for ()
+
+
         }
-    };
+        ;
+        return 3;
+    }
 }
