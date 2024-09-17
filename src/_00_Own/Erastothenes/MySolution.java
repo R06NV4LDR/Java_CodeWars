0f659defc62e4f3d7457ff30394531088641e0d2
R06NV4LDR <ronny.bruhin+github@hotmail.com>
2024-09-17 14:54:23 +0200

    _00_Own.Erastothenes created | Sieve of Erastothenes

---
+++ src/_00_Own/Erastothenes/MySolution.java
@@ -0,0 +1,13 @@
+package _00_Own.Erastothenes;
+
+public class MySolution {
+    public static int sieve(int[] array) {
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,};
+
+        for (int i = 0; i < array.length; i++) {
+            int product = i * i;
+            return product;
+            for ()
+        }
+    };
+}
