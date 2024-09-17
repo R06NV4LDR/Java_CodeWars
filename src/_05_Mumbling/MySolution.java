121a13a4b5518372e63fd802f3ee8e671e6abb34
R06NV4LDR <ronny.bruhin+github@hotmail.com>
2024-09-17 11:51:40 +0200

    _05_Mumbling | Working Emoji Handling

--- src/_05_Mumbling/MySolution.java
+++ src/_05_Mumbling/MySolution.java
@@ -1,20 +1,34 @@
 package _05_Mumbling;
 
+import java.util.StringJoiner;
+
 import static java.io.File.separator;
 
 public class MySolution {
     public static String accum(String s) {
         StringBuilder result = new StringBuilder();
+        int counter = 0;
 
-        for (int i = 0, offset = 0; offset < s.length(); i++) {
+//        s.codePoints()
+//                .map(codPoint -> {
+//                  // magic
+//                })
+//                .collect(StringBuilder::new, "-"):
+        for (int offset = 0; offset < s.length(); ) {
             int codePoint = s.codePointAt(offset);
+            if (Character.isEmojiComponent(codePoint)) {
+                ++offset;
+                continue;
+            }
             int charCount = Character.charCount(codePoint);
 
             result.append(new String(Character.toChars(Character.toUpperCase(codePoint))));
 
-            for (int j = 0; j < i; ++j) {
-                result.append(new String(Character.toChars(Character.toLowerCase(codePoint))));
+            String str = new String(Character.toChars(Character.toLowerCase(codePoint)));
+            for (int j = 0; j < counter; ++j) {
+                result.append(str);
             }
+            ++counter;
 
             if (offset + charCount < s.length()) {
                 result.append("-");
